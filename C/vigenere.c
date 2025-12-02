d5a2b03f6494a528ddbf42606c932f6224cb1b27
Amine Djabri <djabri.aminee@gmail.com>
2025-12-01 21:36:44 -0600

    Makefike

--- C/vigenere.c
+++ C/vigenere.c
@@ -1,5 +1,14 @@
 #include "vigenere.h"
 
+const char alphabet[64] = {
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+    'Q','R','S','T','U','V','W','X','Y','Z',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+    'q','r','s','t','u','v','w','x','y','z',
+    '0','1','2','3','4','5','6','7','8','9',
+    '+','/'
+};
+
 void chiffrer_vigenere(char *texte, const char *cle) {
     const char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     int len = strlen(cle), j = 0; // Récuperer la longueur de la cle 
@@ -17,7 +26,7 @@ void chiffrer_vigenere(char *texte, const char *cle) {
 }
 
 // Trouver la position d'un carctere dans une chaine 
-int position (char caractere, char liste_caractere[],int longeur_liste){
+int position (char caractere, const char liste_caractere[],int longeur_liste){
   for (int i=0; i<longeur_liste; i++){
     if (liste_caractere[i]==caractere){
       return i;
@@ -30,15 +39,7 @@ int position (char caractere, char liste_caractere[],int longeur_liste){
 // Dechiffrement 
 void dechiffrer(char texte[], char cle[]) {
     
-    // Alphabet Base64 
-    char alphabet[64] = {
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-        'Q','R','S','T','U','V','W','X','Y','Z',
-        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
-        'q','r','s','t','u','v','w','x','y','z',
-        '0','1','2','3','4','5','6','7','8','9',
-        '+','/'
-    };
+
 
     int longeur_cle = strlen(cle);
     int j = 0;
--- C/vigenere.h
+++ C/vigenere.h
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+extern const char alphabet[64];
+
 
 void chiffrer_vigenere(char *texte, const char *cle);
 void dechiffrer(char texte[], char cle[]) ;
 char* findkey (char *txt_clair,char*  txt_chiff);
-int position (char caractere, char liste_caractere[],int longeur_liste);
+int position (char caractere,const char liste_caractere[],int longeur_liste);
 int periode(char *chaine, int n);
 char* lire_fichier( char *nom_fichier);
 int ecrire_fichier( char *nom_fichier, char *contenu);
