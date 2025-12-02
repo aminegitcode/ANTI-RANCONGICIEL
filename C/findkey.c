a062679ce2e153b555b7e0b69ce9b2b70f2d2c02
Amine Djabri <djabri.aminee@gmail.com>
2025-12-01 18:17:31 -0600

    findkey

--- C/findkey.c
+++ C/findkey.c
@@ -6,8 +6,16 @@ int main( int argc, int *argv[]){
         return 1;
     }
     
-    char 
+    char *contenu_clair= lire_fichier(argv[1]); //Lire le contenu du fichier clair
+    char *contenu_chiff=lire_fichier(argv[2]); //Lire le contenu du fichier chiffré
     
+    //Trouver la clé
+    char *cle=findkey(contenu_clair,contenu_chiff);
+    
+    
+    printf("La clé est : %s",cle); //Afficher la cle sur la sortie standard
+    int longuer_cle=strlen(cle);
+    fprintf(stderr,"%d",longeur_cle);
 
-
+    free(cle);
 }
