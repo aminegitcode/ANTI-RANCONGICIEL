97c5456f9b1ef0d2bf0065e35a27d472878763cf
Amine Djabri <djabri.aminee@gmail.com>
2025-12-10 18:20:32 +0100

    restore-archive

--- integration/restore-archive.sh
+++ integration/restore-archive.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 
-$fichier_archives="./sh-toolbox/archives"
-
+fichier_archives="./sh-toolbox/archives"
+tmp_dossier=".sh-toolbox/temp" #Dossier temporaire pour decompresser l'archive
+log_fichier="$tmp_dossier/var/log/auth.log"
+admin_tmp=".sh-toolbox/admin_tmp" #
 
 if [ $# -ne 1 ];then
 	echo "Erreur d'usage: ./restore-archive.sh <chemin>"
+	exit 1
+fi
+
+#Verifier l'existence du dossier .sh-toolbox
+if [ ! -d "./sh-toolbox" ] ; then
+	echo ".sh-toolbox n'existe pas "
 	exit 1
 fi
 
@@ -42,11 +50,63 @@ while IFS=":" read nom reste; do
 	fi
 	
 	if [ $i -eq $choix ]; then
-		nom_archive=$nom
+		nom_archive=$nom #Recuperer le nom de l'archive
 		break
 	fi
 	i=$((i+1))
 done < "$fichier_archives"
 
+archive="$dossier/$nom_archive" #Chemin de l'archive 
+
+# Verifier l'existence de cette archive dans le dossier .sh-toolbox
+if [ ! -f "$archive" ]; then
+    echo "Erreur : l'archive '$nom_archive' n'existe pas dans $dossier."
+    exit 2
+fi
+
+# Creer le dossier temporaire s'il n'existe pas
+if [ ! -d $tmp_dossier ];then
+	mkdir  "$tmp_dossier"
+fi
+
+# Decompression de l’archive dans le dossier temporaire.
+tar -xzf "$archive" -C "$tmp_dossier" #extraire l’archive 
+if [ $? -ne 0 ]; then
+    echo "Erreur : echec de la décompression."
+    exit 3
+    rm -rf "$tmp_dossier" # Suppression de tmp_dossier
+fi
 
 
+
+# Verifier l'existence du fichier Log 
+if [ ! -f "$log_fichier" ]; then
+    echo "Erreur : fichier de logs introuvable."
+    exit 4
+    rm -rf "$tmp_dossier"
+fi
+
+
+# Récuperer toutes les tentatives de connexions réussies de admin et copier la derniere tentative dans la variable "admin"
+admin=$(grep "Accepted password for admin" "$log_fichier" | tail -n 1)
+
+
+# Récuperer la date et l'heure de la derniere connexion de admin
+mois=$(echo "$admin" | cut -d' ' -f1)
+jour=$(echo "$admin" | cut -d' ' -f2)
+heure=$(echo "$admin" | cut -d' ' -f3)
+
+
+find "$data_dossier" -type f > "$data_tmp" # Copier tous les fichiers qui existent dans le dossier data dans un fichier temporaire
+
+
+
+
+
+
+
+
+
+
+rm -rf "$tmp_dossier" # Suppression de tmp_dossier
+exit 0
